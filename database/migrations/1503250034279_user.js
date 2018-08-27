a565425381ee59735cab11e210740758d96901ef
Pedro Henrique <pedro.lg.cs@gmail.com>
2018-08-27 16:57:19 -0300

    create user migration

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -6,9 +6,11 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.string('username', 50).notNullable().unique()
       table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
+      table.string('password', 50).notNullable()
+      table.boolean('active').defaultTo(false).notNullable()
+      table.enu('user_type', ['admin', 'client', 'user']).notNullable()
       table.timestamps()
     })
   }
